01f4dfab431fda282d98bab424b0ff53952b5b42
Marcos Gutierrez <marcose.gutierrez@hotmail.com>
2023-09-02 09:34:19 -0300

    Rutas y conecciones

--- router.js
+++ router.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const { conexion, connection } = require('./database/db')
+const connection = require('./database/db')
 
 router.get('/', async (req, res) => {
     try {
         const [results] = await connection.query('SELECT * FROM users');
-        console.log(results)
         res.render('index', {results: results});
     } catch (error) {
         return res.status(500).json({ message: "Algo salió mal"});
@@ -17,26 +16,24 @@ router.get('/create', (req, res) => {
     res.render('create')
 })
 
-router.get('/edit/:id', (req, res) => {
+router.get('/edit/:id', async (req, res) => {
     const id = req.params.id
-    conexion.query('SELECT * FROM users WHERE id=?', [id], (error, results) => {
-        if(error) {
-            throw error
-        } else {
-            res.render('edit', {user:results[0]})
-        }
-    })
+    try {
+        const [results] = await connection.query('SELECT * FROM users WHERE id=?', [id]);
+        res.render('edit', {user:results[0]});
+    } catch (error) {
+        return res.status(500).json({ message: "Algo salió mal"});
+    }
 })
 
-router.get('/delete/:id', (req, res) => {
+router.get('/delete/:id', async (req, res) => {
     const id = req.params.id
-    conexion.query('DELETE FROM users WHERE id = ?', [id], (error, results) => {
-        if(error) {
-            throw error
-        } else {
-            res.redirect('/')
-        }
-    })
+    try {
+        await connection.query('DELETE FROM users WHERE id = ?', [id]);
+        res.redirect('/');
+    } catch (error) {
+        return res.status(500).json({ message: "Algo salió mal"});
+    }
 })
 
 const crud = require('./controllers/crud')
