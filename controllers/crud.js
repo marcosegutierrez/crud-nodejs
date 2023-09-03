0810d5505cf12fcb2e21ede84245bf398f7f7254
Marcos Gutierrez <marcose.gutierrez@hotmail.com>
2023-09-03 18:07:13 -0300

    Modificaciones en controladores de guardado y actualizado

--- controllers/crud.js
+++ controllers/crud.js
@@ -1,26 +1,24 @@
-const { conexion, connection } = require('../database/db')
+const connection = require('../database/db')
 
-exports.save = (req, res) => {
+exports.save = async (req, res) => {
     const user = req.body.user
     const rol = req.body.rol
-    conexion.query('INSERT INTO users SET ?', {user:user, rol:rol}, (error, results) => {
-        if(error) {
-            console.log(error)
-        } else {
-            res.redirect('/')
-        }
-    })
+    try {
+        await connection.query('INSERT INTO users SET ?', {user:user, rol:rol});
+        res.redirect('/');
+    } catch (error) {
+        return res.status(500).json({message: "Error al guardar usuario"});
+    }
 }
 
-exports.update = (req, res) => {
+exports.update = async (req, res) => {
     const id = req.body.id
     const user = req.body.user
     const rol = req.body.rol
-    conexion.query('UPDATE users SET ? WHERE id = ?', [{user:user, rol:rol}, id], (error, results) => {
-        if(error) {
-            console.log(error)
-        } else {
-            res.redirect('/')
-        }
-    })
+    try {
+        await connection.query('UPDATE users SET ? WHERE id = ?', [{user:user, rol:rol}, id]);
+        res.redirect('/');
+    } catch (error) {
+        return res.status(500).json({message: "Error al actualizar usuario"});
+    }
 }
